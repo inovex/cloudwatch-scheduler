383b8103109112ab0b84fb126709ab0ababf6f03
Danny Textores <textores.danny@gmail.com>
2020-08-03 14:49:57 +0200

    implement dynamo crud repo

---
+++ scheduling/dynamo.go
@@ -0,0 +1,39 @@
+package scheduling
+
+import (
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/guregu/dynamo"
+	"task-editor/models"
+)
+
+type dynamoRepo struct {
+	table dynamo.Table
+}
+
+func newDynamoRepo(taskTable string) dynamoRepo {
+	db := dynamodb.New(session.Must(session.NewSessionWithOptions(session.Options{})))
+	t := dynamo.NewFromIface(db).Table(taskTable)
+	return dynamoRepo{table: t}
+}
+
+func (r dynamoRepo) GetTasks() ([]models.Task, error) {
+	var out []models.Task
+	err := r.table.
+		Scan().
+		All(&out)
+	return out, err
+}
+
+func (r dynamoRepo) AddTask(task models.Task) error {
+	return r.table.
+		Put(task).
+		Run()
+}
+
+func (r dynamoRepo) DeleteTask(id string) error {
+	return r.table.
+		Delete("ID", id).
+		Run()
+}
+
